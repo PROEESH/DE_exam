dc0468c4de76bc9f3fd7fcf86abad34937ef8711
Your Name <you@example.com>
2025-11-12 21:51:54 +0200

    new

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 print("Hello, World!")
 print("This is the main script for the DE exam project.")
-print("Add your data engineering code here   .")
+print("Add your data engineering code here    .")
