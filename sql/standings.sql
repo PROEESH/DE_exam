aa66588b2603f165ca075eb80081ace20e798641
Your Name <you@example.com>
2025-11-15 17:50:38 +0200

    aaa

--- sql/standings.sql
+++ sql/standings.sql
@@ -1,3 +1,3 @@
 -- Silver layer: union teams from both APIs
 CREATE OR REPLACE TABLE `voltaic-tooling-471807-t5.teams.standings` AS
-SELECT leauge_id, team_id, overall_league_position FROM `voltaic-tooling-471807-t5.teams.standings_API2`
+SELECT league_id, team_id, overall_league_position FROM `voltaic-tooling-471807-t5.teams.standings_API2`
